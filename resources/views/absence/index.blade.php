40d08d85bc8f0c2166cab34abdf64c178e8bb811
Accendit <36697826+Accendit@users.noreply.github.com>
2020-01-21 14:33:19 +0100

    Implemented policies in view

--- resources/views/absence/index.blade.php
+++ resources/views/absence/index.blade.php
@@ -28,34 +28,35 @@
                         </thead>
                         <tbody>
                             @foreach ($absences as $absence)
-                            <tr>
-                                <th>{{ $absence->id }}</th>
-                                <td>{{ $absence->submitter()->name }}</td>
-                                <td>{{ $absence->startdate }}</td>
-                                <td>{{ $absence->enddate }} </td>
-                                @isset ($absence->isapproved)
-                                    <td>{{ $absence->isapproved? "Ja" : "Nee" }}</td>
-                                @else
-                                    <td>Nog niet beoordeeld</td>
-                                @endisset
-                                
-                                
-                                
-                                <td>
-                                    <div class="btn-group" role="group">
-                                        @if (Auth::User()->isManager() && !$absence->isapproved)
-                                            <form action="/absences/{{ $absence->id }}/approve" method="post">
-                                                <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
-                                                <button class="btn btn-primary" type="submit">Goedkeuren</button>
-                                            </form>
-                                        @endif
-                                        @if (Auth::User()->id === $absence->submitter()->id && !$absence->isapproved)
-                                            <button class="btn btn-secondary">Bewerk</button>
-                                        @endif
-                                    </div>
-                                </td>
-                                
-                            </tr>
+                                    <tr>
+                                        <th>{{ $absence->id }}</th>
+                                        <td>{{ $absence->submitter()->name }}</td>
+                                        <td>{{ $absence->startdate }}</td>
+                                        <td>{{ $absence->enddate }} </td>
+                                        @isset ($absence->isapproved)
+                                            <td>{{ $absence->isapproved? "Ja" : "Nee" }}</td>
+                                        @else
+                                            <td>Nog niet beoordeeld</td>
+                                        @endisset
+                                        
+                                        
+                                        
+                                        <td>
+                                            <div class="btn-group" role="group">
+                                                @can('approve', $absence)
+                                                    <form action="/absences/{{ $absence->id }}/approve" method="post">
+                                                        <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
+                                                        <button class="btn btn-primary" type="submit">Goedkeuren</button>
+                                                    </form>
+                                                @endcan
+                                                @can('update', $absence)
+                                                    <a class="btn btn-secondary" href="/absences/{{ $absence->id }}/edit">Bewerk</a>
+                                                @endcan
+                                            </div>
+                                        </td>
+                                        
+                                    </tr>
+                           
                             @endforeach
                         </tbody>
                     </table>
